86a5bc7a1aa9d7ca9974c6711ca66710cbddf94c
barringtonhulana <barringtonhulana@gmail.com>
2023-02-27 16:14:25 +0200

    Dev | Update store method in CareerController

--- app/Http/Controllers/CareerController.php
+++ app/Http/Controllers/CareerController.php
@@ -11,14 +11,6 @@ use Inertia\Inertia;
 class CareerController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new career lead.
      * 
      * @return Inertia\Response
@@ -33,58 +25,24 @@ class CareerController extends Controller
      */
     public function store(StoreCareerRequest $request)
     {
-
-        $addressId = Address::create([
-            'country' => $request->validated()['country'],
-            'street_address' => $request->validated()['street_address'],
-            'city' => $request->validated()['city'],
-            'region' => $request->validated()['region'],
-            'postal_code' => $request->validated()['postal_code'],
-        ])->id;
-
-        Career::create([
+        $career = Career::create([
             'first_name' => $request->validated()['first_name'],
             'last_name' => $request->validated()['last_name'],
             'email' => $request->validated()['email'],
-            'address_id' => $addressId,
             'phone_number' => $request->validated()['phone_number'],
             'referred_via' => $request->validated()['referred_via'],
             'opt_in_marketing' => $request->validated()['opt_in_marketing'],
             'accepted_terms_and_conditions' => $request->validated()['accepted_terms_and_conditions'],
         ]);
 
+        $career->address()->create([
+            'country' => $request->validated()['country'],
+            'street_address' => $request->validated()['street_address'],
+            'city' => $request->validated()['city'],
+            'region' => $request->validated()['region'],
+            'postal_code' => $request->validated()['postal_code'],
+        ]);
+
         return redirect()->route('career.create');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Career $career)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Career $career)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateCareerRequest $request, Career $career)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Career $career)
-    {
-        //
     }
 }
