f2af3069ae59533ce363e7fb87fb1c32567aa4fd
barringtonhulana <barringtonhulana@gmail.com>
2023-02-27 14:53:20 +0200

    Dev | Update address_id to nullable

--- database/migrations/2023_02_24_060453_create_careers_table.php
+++ database/migrations/2023_02_24_060453_create_careers_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('last_name', 100);
             $table->string('email')->unique();
             $table->foreignId('address_id')
+                ->nullable()
                 ->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
